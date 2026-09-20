d263dbcca9f143ceb4a0548396d4922da7b41590
HenriqueBossle <henriquegoncalvesbossle@gmail.com>
2026-09-19 22:21:49 -0300

    add controller, migration and articlestorerequest

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ArticleStoreRequest;
+use App\Models\Article;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ArticleController extends Controller
 {
-    //
+
+    public function index(): JsonResponse
+    {
+        if (!Auth::check()) {
+            return response()->json([
+                'message' => 'Unauthenticated',
+            ], 401);
+        }
+
+        $articles = Article::latest()->paginate(10);
+
+        return response()->json([
+            'data' => $articles,
+        ]);
+    }
+
+    public function store(ArticleStoreRequest $request)
+    {
+        $article = Article::create($request->validated());
+
+        return response()->json($article, 201);
+    }
+
+    
 }
