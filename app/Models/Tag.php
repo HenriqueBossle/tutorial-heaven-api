5f4e38519612063cd55ab08359e01d8da449a4c7
arthur <arthurprotti51@gmail.com>
2026-09-22 20:26:26 -0300

    feat: adiciona formulario de tags e rota de onboarding

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -1,13 +1,25 @@
 <?php
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+    use HasFactory;
+
+    // Adicione esta propriedade liberando os campos para escrita em massa
+    protected $fillable = [
+        'name',
+        'slug',
+        'category',
+        'description',
+        'display_order',
+        'is_active',
+    ];
+
     public function users()
-{
-    return $this->belongsToMany(User::class)->withTimestamps();
-}
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
 }
